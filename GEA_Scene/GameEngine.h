189fe8145944984e20d54bcec3ca6249abf5e115
PhantaBCU <113676826+PhantaBCU@users.noreply.github.com>
2024-01-25 14:44:07 +0000

    second commit (finished task1)

---
+++ GEA_Scene/GameEngine.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <glew.h>
+#include <SDL.h>
+#include <SDL_opengl.h>
+
+namespace GE {
+
+	// Basic Game Engine class
+	class GameEngine {
+	public:
+		GameEngine();			// Constructor
+
+		virtual ~GameEngine();	// Destructor
+
+		bool init(bool vSyncEnabled);		// Object initialisation
+		bool keep_running();				// Indicates if user has closed window and, hence, game
+		void update();						// This is where we update game logic
+		void draw();						// This is where a frame is rendered
+		void shutdown();					// This is called when the game is ending
+		void clean();						// to release objects created during the game
+
+		// Extra methods
+		void setwindowtitle(const char*);
+
+	private:
+		// Private data
+		// SDL window object to represent the frame
+		SDL_Window* window = nullptr;
+
+		// SDL Gl context to represent OpenGL in the program
+		SDL_GLContext glContext = nullptr;
+	};
+
+	// Helper function
+	void display_info_message(const char*);
+
+}
+
+
+
+
---
+++ GEA_Scene/main.cpp
@@ -0,0 +1,49 @@
+#define SDL_MAIN_HANDLED
+#include "GameEngine.h"
+// ADD THIS CODE FOR TASK 2
+#include <sstream>
+
+using namespace GE;
+
+int main(int argc, char *argv[]) {
+	// Create a game engine object
+	GameEngine ge;
+
+	// Initialise it
+	if (!ge.init(true)) {
+		display_info_message("Couldn't start SDL.  Check console output for more details");
+
+		return -1;
+	}
+
+	Uint32 last_time = SDL_GetTicks(), current_time = 0;
+	int frame_count = 0;
+
+	// Main loop
+	while (ge.keep_running()) {
+		// Update game state
+		ge.update();
+
+		// Draw
+		ge.draw();
+
+		frame_count++;
+
+		current_time = SDL_GetTicks();
+
+		if (current_time - last_time > 1000) {
+			//Update window title once per second with FPS information
+			std::ostringstream msg;
+			msg << "FPS = " << frame_count;
+			ge.setwindowtitle(msg.str().c_str());
+
+			frame_count = 0;
+			last_time = current_time;
+		}
+	}
+
+	// Destroy game engine
+	ge.shutdown();
+
+	return 0;
+}
